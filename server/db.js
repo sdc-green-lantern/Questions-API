a5d0446ba5b3d3234dda27c7d5f285e03ad1e765
Daniel Tan <dantan510@gmail.com>
2022-09-23 12:34:03 -0700

    change server

--- server/db.js
+++ server/db.js
@@ -1,13 +1,24 @@
-const { Client } = require('pg')
+const { Pool } = require('pg')
+require("dotenv").config();
 
-const client = new Client({
-  host: 'localhost',
-  user: 'dan',
-  password: '',
-  database: 'qa'
+// const client = new Client({
+//   host: 'localhost',
+//   user: 'dan',
+//   password: '',
+//   database: 'qa',
+//   port: 5432
+// })
+
+const client = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDB,
+  password: process.env.PGPASS,
+  port: process.env.PGPORT
 })
 
 client.connect()
 .then(() => console.log('connected to postgres'))
+.catch((err) => console.log('ERROR : ', err))
 
 module.exports = { client }
